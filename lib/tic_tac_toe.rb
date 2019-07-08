12b5e28f56d82873094ac89bae89eeab217d27a1
Louis Raymond <louis.raymond@alumni.york.ac.uk>
2019-07-08 16:30:42 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,19 @@
+class TicTacToe
+  WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]]
+  def initialize
+    @board = [" "," "," "," "," "," "," "," "," "]
+  end
+  
+  def input_to_index(input)
+    input= gets.chomp
+    input= input.to_i - 1
+  end
+  
+  def display_board
+    puts " " + @board[0] + " | " + @board[1] + " | " + @board[2] + " "
+    puts "-----------"
+    puts " " + @board[3] + " | " + @board[4] + " | " + @board[5] + " "
+    puts "-----------"    
+    puts " " + @board[6] + " | " + @board[7] + " | " + @board[8] + " "
+  end
+end
